862757a5e71d5ef48eeba46fa709e8c6e21ddd99
Amelia <amelia@egg.fish>
2026-02-11 21:38:45 -0800

    Got all of those nasty allocations out of the renderer hot loop

--- engine/src/renderer.rs
+++ engine/src/renderer.rs
@@ -21,7 +21,25 @@ pub struct Renderer {
     gl: Rc<GlowContext>,
     frames_rendered: u64,
     vao_cache: HashMap<VaoKey, glow::VertexArray>,
+    frame_data: FrameData,
 }
+
+#[derive(Default)]
+struct FrameData {
+    visible_instances: Vec<RenderInstance>,
+    instances_by_material: HashMap<MaterialHandle, Vec<RenderInstance>>,
+    instances_by_mesh: HashMap<MeshHandle, Vec<[f32; 16]>>,
+    frame_uniforms: FrameUniforms,
+}
+
+#[derive(Default)]
+struct FrameUniforms {
+    view_proj: Mat4,
+    camera_position: Vec3,
+    light_direction: Vec3,
+    light_color: Vec3,
+}
+
 pub struct RenderParams {
     pub width: u32,
     pub height: u32,
@@ -46,8 +64,265 @@ impl Renderer {
         x.max(y).max(z)
     }
 
-    fn get_or_create_vao(
+    pub fn render(
         &mut self,
+        render_params: RenderParams,
+        render_data_manager: &mut RenderResourceManager,
+        instances: Vec<RenderInstance>,
+        camera: Option<CameraRenderData>,
+    ) {
+        let gl = &self.gl;
+        let current_time = std::time::Instant::now();
+        let mut draw_calls = 0;
+
+        unsafe {
+            gl.enable(glow::DEPTH_TEST);
+            gl.depth_func(glow::LEQUAL);
+            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
+            gl.enable(glow::CULL_FACE);
+            gl.cull_face(glow::BACK);
+        }
+
+        let saved_viewport = {
+            let mut vp = [0; 4];
+            unsafe {
+                gl.get_parameter_i32_slice(glow::VIEWPORT, &mut vp);
+            }
+            vp
+        };
+
+        unsafe {
+            gl.viewport(
+                0,
+                0,
+                render_params.width as i32,
+                render_params.height as i32,
+            );
+        }
+
+        let Some(camera) = camera else {
+            unsafe {
+                gl.viewport(
+                    saved_viewport[0],
+                    saved_viewport[1],
+                    saved_viewport[2],
+                    saved_viewport[3],
+                );
+                return;
+            }
+        };
+
+        let view_proj = camera.view_proj;
+
+        self.frame_data.visible_instances.clear();
+        self.frame_data.instances_by_material.clear();
+        self.frame_data.instances_by_mesh.clear();
+
+
+        // Set common uniforms
+        let default_light_color = Vec3::new(1.0, 1.0, 1.0);
+
+        self.frame_data.frame_uniforms.view_proj = view_proj;
+        self.frame_data.frame_uniforms.camera_position = camera.position;
+        self.frame_data.frame_uniforms.light_direction = Vec3::new(0.0, 0.0, 1.0);
+        self.frame_data.frame_uniforms.light_color = default_light_color;
+
+
+        // ------------------------------------------------------------
+        // PASS 1: Frustum culling
+        // ------------------------------------------------------------
+        let frustum = Frustum::from_view_proj(&view_proj);
+
+        for inst in instances {
+            // println!("Checking culling for instance: {:?}", inst);
+            let mesh = render_data_manager
+                .mesh_manager
+                .get_mesh(inst.mesh_id)
+                .expect("Mesh not found");
+
+            let scale = Self::max_scale(inst.transform);
+            let world_center = inst
+                .transform
+                .transform_point3(Vec3::from(mesh.sphere_center));
+            let world_radius = mesh.sphere_radius * scale;
+
+            if frustum.intersects_sphere(world_center, world_radius) {
+                self.frame_data.visible_instances.push(inst);
+            }
+        }
+
+        // ------------------------------------------------------------
+        // PASS 2: Group by material
+        // ------------------------------------------------------------
+
+        for inst in self.frame_data.visible_instances.drain(..) {
+            self.frame_data
+                .instances_by_material
+                .entry(inst.material_id)
+                .or_default()
+                .push(inst);
+        }
+
+        // ------------------------------------------------------------
+        // PASS 3: Render
+        // ------------------------------------------------------------
+        for (material_id, inst_group) in &self.frame_data.instances_by_material {
+            let material = render_data_manager
+                .material_manager
+                .get_material(*material_id)
+                .expect("Material not found");
+            let shader = render_data_manager
+                .shader_manager
+                .get_shader(material.desc.shader)
+                .expect("Shader not found");
+
+            // Bind shader
+            unsafe {
+                gl.use_program(Some(shader.program));
+                material.desc.params.keys().for_each(|name| {
+                    if !shader.uniforms.contains_key(name) {
+                        println!(
+                            "Warning: material provides uniform '{}' not used by shader",
+                            name
+                        );
+                    }
+                });
+            }
+
+            // 1. Bind frame uniforms
+            if let Some(loc) = shader.uniforms.get("u_view_proj") {
+                Self::bind_uniform(
+                    gl,
+                    loc,
+                    &UniformValue::Mat4(self.frame_data.frame_uniforms.view_proj),
+                    render_data_manager,
+                );
+            }
+            if let Some(loc) = shader.uniforms.get("u_camera_position") {
+                Self::bind_uniform(
+                    gl,
+                    loc,
+                    &UniformValue::Vec3(self.frame_data.frame_uniforms.camera_position),
+                    render_data_manager,
+                );
+            }
+            if let Some(loc) = shader.uniforms.get("u_light_direction") {
+                Self::bind_uniform(
+                    gl,
+                    loc,
+                    &UniformValue::Vec3(self.frame_data.frame_uniforms.light_direction),
+                    render_data_manager,
+                );
+            }
+            if let Some(loc) = shader.uniforms.get("u_light_color") {
+                Self::bind_uniform(
+                    gl,
+                    loc,
+                    &UniformValue::Vec3(self.frame_data.frame_uniforms.light_color),
+                    render_data_manager,
+                );
+            }
+
+            let mut textures_bound: u32 = 0;
+
+            // 2. Bind material uniforms
+            for (name, value) in &material.desc.params {
+                if let Some(loc) = shader.uniforms.get(name) {
+                    textures_bound += Self::bind_uniform(gl, loc, value, render_data_manager);
+                }
+            }
+
+            // Group by mesh
+
+            self.frame_data.instances_by_mesh.clear();
+            for inst in inst_group {
+                self.frame_data
+                    .instances_by_mesh
+                    .entry(inst.mesh_id)
+                    .or_default()
+                    .push(inst.transform.to_cols_array());
+            }
+
+            // Draw each mesh
+            for (mesh_id, matrices) in &self.frame_data.instances_by_mesh {
+                let vao = Self::get_or_create_vao(
+                    &mut self.vao_cache,
+                    &self.gl,
+                    &mesh_id,
+                    &material.desc.shader,
+                    render_data_manager,
+                );
+
+                let index_count = {
+                    let mesh = render_data_manager
+                        .mesh_manager
+                        .get_mesh_mut(*mesh_id)
+                        .expect("Mesh not found");
+                    mesh.update_instance_buffer(&matrices, gl);
+                    mesh.indices.len() as i32
+                };
+
+                unsafe {
+                    gl.bind_vertex_array(Some(vao));
+                    gl.draw_elements_instanced(
+                        glow::TRIANGLES,
+                        index_count,
+                        glow::UNSIGNED_INT,
+                        0,
+                        matrices.len() as i32,
+                    );
+                }
+                draw_calls += 1;
+            }
+            // Now that we are done with the material, unbind any textures it used
+            for unit in 0..textures_bound {
+                unsafe {
+                    gl.active_texture(glow::TEXTURE0 + unit);
+                    gl.bind_texture(glow::TEXTURE_2D, None);
+                }
+            }
+        }
+
+        unsafe {
+            gl.bind_vertex_array(None);
+            gl.viewport(
+                saved_viewport[0],
+                saved_viewport[1],
+                saved_viewport[2],
+                saved_viewport[3],
+            );
+        }
+
+        self.frames_rendered += 1;
+
+        if self.frames_rendered % 60 == 0 {
+            println!("Frames rendered: {}", self.frames_rendered);
+            println!(
+                "Render time: {:.2} ms",
+                current_time.elapsed().as_secs_f32() * 1000.0
+            );
+            println!("Draw calls on last frame: {}", draw_calls);
+        }
+    }
+
+    pub fn new(gl: Rc<GlowContext>) -> Self {
+        unsafe {
+            gl.enable(glow::DEPTH_TEST);
+            gl.depth_func(glow::LESS);
+
+            let renderer = Self {
+                gl,
+                frames_rendered: 0,
+                vao_cache: HashMap::new(),
+                frame_data: FrameData::default(),
+            };
+            renderer
+        }
+    }
+
+    fn get_or_create_vao(
+        vao_cache: &mut HashMap<VaoKey, glow::VertexArray>,
+        gl: &glow::Context,
         mesh: &MeshHandle,
         shader: &ShaderHandle,
         render_data_manager: &RenderResourceManager,
@@ -57,12 +332,11 @@ impl Renderer {
             shader: *shader,
         };
 
-        if let Some(vao) = self.vao_cache.get(&key) {
+        if let Some(vao) = vao_cache.get(&key) {
             return *vao;
         }
 
         unsafe {
-            let gl = &self.gl;
             let vao = gl.create_vertex_array().unwrap();
             gl.bind_vertex_array(Some(vao));
 
@@ -150,235 +424,8 @@ impl Renderer {
             }
 
             gl.bind_vertex_array(None);
-            self.vao_cache.insert(key, vao);
+            vao_cache.insert(key, vao);
             vao
-        }
-    }
-
-    pub fn render(
-        &mut self,
-        _render_params: RenderParams,
-        render_data_manager: &mut RenderResourceManager,
-        instances: Vec<RenderInstance>,
-        camera: Option<CameraRenderData>,
-    ) {
-        let gl = &self.gl.clone();
-        let current_time = std::time::Instant::now();
-        let mut draw_calls = 0;
-
-        unsafe {
-            gl.enable(glow::DEPTH_TEST);
-            gl.depth_func(glow::LEQUAL);
-            gl.clear(glow::COLOR_BUFFER_BIT | glow::DEPTH_BUFFER_BIT);
-            gl.enable(glow::CULL_FACE);
-            gl.cull_face(glow::BACK);
-
-            // let saved_viewport = {
-            //     let mut vp = [0; 4];
-            //     gl.get_parameter_i32_slice(glow::VIEWPORT, &mut vp);
-            //     vp
-            // };
-
-            // gl.viewport(
-            //     0,
-            //     0,
-            //     render_params.width as i32,
-            //     render_params.height as i32,
-            // );
-
-            // let Some(camera) = camera else {
-            //     gl.viewport(
-            //         saved_viewport[0],
-            //         saved_viewport[1],
-            //         saved_viewport[2],
-            //         saved_viewport[3],
-            //     );
-            //     return;
-            // };
-        }
-        let camera = camera.unwrap();
-
-        let view_proj = camera.view_proj;
-
-        // ------------------------------------------------------------
-        // PASS 1: Frustum culling
-        // ------------------------------------------------------------
-        let frustum = Frustum::from_view_proj(&view_proj);
-
-        let mut visible_instances: Vec<&RenderInstance> = Vec::new();
-
-        for inst in &instances {
-            // println!("Checking culling for instance: {:?}", inst);
-            let mesh = render_data_manager
-                .mesh_manager
-                .get_mesh(inst.mesh_id)
-                .expect("Mesh not found");
-
-            let scale = Self::max_scale(inst.transform);
-            let world_center = inst
-                .transform
-                .transform_point3(Vec3::from(mesh.sphere_center));
-            let world_radius = mesh.sphere_radius * scale;
-
-            if frustum.intersects_sphere(world_center, world_radius) {
-                visible_instances.push(inst);
-            }
-        }
-
-        // ------------------------------------------------------------
-        // PASS 2: Group by material
-        // ------------------------------------------------------------
-        use std::collections::HashMap;
-        let mut instances_by_material: HashMap<MaterialHandle, Vec<&RenderInstance>> =
-            HashMap::new();
-        for inst in &visible_instances {
-            instances_by_material
-                .entry(inst.material_id)
-                .or_default()
-                .push(inst);
-        }
-
-        // Set common uniforms
-        let default_light_color = Vec3::new(1.0, 1.0, 1.0);
-        let mut frame_uniforms = HashMap::<String, UniformValue>::new();
-        frame_uniforms.insert("u_view_proj".into(), UniformValue::Mat4(view_proj));
-        frame_uniforms.insert(
-            "u_camera_position".into(),
-            UniformValue::Vec3(camera.position),
-        );
-        frame_uniforms.insert(
-            "u_light_direction".into(),
-            UniformValue::Vec3(Vec3::new(0.0, 0.0, 1.0)),
-        );
-        frame_uniforms.insert(
-            "u_light_color".into(),
-            UniformValue::Vec3(default_light_color),
-        );
-
-        // ------------------------------------------------------------
-        // PASS 3: Render
-        // ------------------------------------------------------------
-        for (material_id, inst_group) in instances_by_material {
-            let material = render_data_manager
-                .material_manager
-                .get_material(material_id)
-                .expect("Material not found");
-            let shader = render_data_manager
-                .shader_manager
-                .get_shader(material.desc.shader)
-                .expect("Shader not found");
-
-            // Bind shader
-
-            unsafe {
-                gl.use_program(Some(shader.program));
-                material.desc.params.keys().for_each(|name| {
-                    if !shader.uniforms.contains_key(name) {
-                        println!(
-                            "Warning: material provides uniform '{}' not used by shader",
-                            name
-                        );
-                    }
-                });
-            }
-
-            // 1. Bind frame uniforms
-            for (name, value) in &frame_uniforms {
-                if let Some(loc) = shader.uniforms.get(name) {
-                    Self::bind_uniform(gl, loc, value, render_data_manager);
-                }
-            }
-
-            let mut textures_bound: u32 = 0;
-
-            // 2. Bind material uniforms
-            for (name, value) in &material.desc.params {
-                if let Some(loc) = shader.uniforms.get(name) {
-                    textures_bound += Self::bind_uniform(gl, loc, value, render_data_manager);
-                }
-            }
-
-            // Group by mesh
-            let mut instances_by_mesh: HashMap<MeshHandle, Vec<[f32; 16]>> = HashMap::new();
-            for inst in inst_group {
-                instances_by_mesh
-                    .entry(inst.mesh_id)
-                    .or_default()
-                    .push(inst.transform.to_cols_array());
-            }
-
-            // Draw each mesh
-            for (mesh_id, matrices) in instances_by_mesh {
-                let vao = Self::get_or_create_vao(
-                    self,
-                    &mesh_id,
-                    &material.desc.shader,
-                    render_data_manager,
-                );
-
-                let index_count = {
-                    let mesh = render_data_manager
-                        .mesh_manager
-                        .get_mesh_mut(mesh_id)
-                        .expect("Mesh not found");
-                    mesh.update_instance_buffer(&matrices, gl);
-                    mesh.indices.len() as i32
-                };
-
-                unsafe {
-                    gl.bind_vertex_array(Some(vao));
-                    gl.draw_elements_instanced(
-                        glow::TRIANGLES,
-                        index_count,
-                        glow::UNSIGNED_INT,
-                        0,
-                        matrices.len() as i32,
-                    );
-                }
-                draw_calls += 1;
-            }
-            // Now that we are done with the material, unbind any textures it used
-            for unit in 0..textures_bound {
-                unsafe {
-                    gl.active_texture(glow::TEXTURE0 + unit);
-                    gl.bind_texture(glow::TEXTURE_2D, None);
-                }
-            }
-        }
-
-        unsafe {
-            gl.bind_vertex_array(None);
-            // gl.viewport(
-            //     saved_viewport[0],
-            //     saved_viewport[1],
-            //     saved_viewport[2],
-            //     saved_viewport[3],
-            // );
-        }
-
-        self.frames_rendered += 1;
-
-        if self.frames_rendered % 60 == 0 {
-            println!("Frames rendered: {}", self.frames_rendered);
-            println!(
-                "Render time: {:.2} ms",
-                current_time.elapsed().as_secs_f32() * 1000.0
-            );
-            println!("Draw calls on last frame: {}", draw_calls);
-        }
-    }
-
-    pub fn new(gl: Rc<GlowContext>) -> Self {
-        unsafe {
-            gl.enable(glow::DEPTH_TEST);
-            gl.depth_func(glow::LESS);
-
-            let renderer = Self {
-                gl,
-                frames_rendered: 0,
-                vao_cache: HashMap::new(),
-            };
-            renderer
         }
     }
 
